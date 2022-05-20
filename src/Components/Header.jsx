fc69d3d2dfe79e611094a0afdb3341efc97ddfda
Tina <hristinadimitrovska@hotmail.com>
2022-05-20 17:26:21 +0200

    commit on master

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,4 +1,3 @@
-import { Offcanvas } from "bootstrap";
 import React from "react";
 import {
   Navbar,
@@ -7,7 +6,6 @@ import {
   Nav,
   NavDropdown,
   Button,
-  // Offcanvas,
 } from "react-bootstrap";
 
 export default function Header() {
@@ -19,47 +17,28 @@ export default function Header() {
           <NavbarBrand href="#">
             <img src={require("../../src/logo-header.png")} alt="" />
           </NavbarBrand>
-          {/* <Navbar.Toggle aria-controls="navbarToggleExternalContent"> */}
-            {/* <span className="navbar-toggler-icon"></span> */}
-            {/* <Navbar.Collapse id="navbarToggleExternalContent"> */}
-              {/* <Navbar.Offcanvas */}
-                {/* // id="navbarToggleExternalContent" */}
-                {/* // aria-labelledby="navbarToggleExternalContent" */}
-                {/* // placement="end */}
-                {/* // " */}
-              {/* // > */}
-                {/* <button */}
-                  {/* // type="button" */}
-                  {/* // className="btn-close text-reset" */}
-                  {/* // data-bs-dismiss="offcanvas" */}
-                  {/* // aria-label="Close" */}
-                {/* // ></button> */}
-                <Nav className="me-auto">
-                  <NavDropdown className="mr-3" title="Product">
-                    <NavDropdown.Item href="#">Overview</NavDropdown.Item>
-                    <NavDropdown.Item href="#">Feautures</NavDropdown.Item>
-                    <NavDropdown.Item href="#">Plans</NavDropdown.Item>
-                  </NavDropdown>
-                  <NavDropdown className="mr-3" title="Resources">
-                    <NavDropdown.Item href="#">Solutions</NavDropdown.Item>
-                    <NavDropdown.Item href="#">NewsRoom</NavDropdown.Item>
-                  </NavDropdown>
-                  <NavDropdown className="mr-3" title="Company">
-                    <NavDropdown.Item href="#">About</NavDropdown.Item>
-                    <NavDropdown.Item href="#">Team</NavDropdown.Item>
-                    <NavDropdown.Item href="#">Careers</NavDropdown.Item>
-                    <NavDropdown.Item href="#">Advertise</NavDropdown.Item>
-                    <NavDropdown.Item href="#">Contact us</NavDropdown.Item>
-                  </NavDropdown>
-                </Nav>
-                <a href="#" className="hov">
-                  Log in
-                </a>
-                <Button className="started">Get started</Button>
-                {/* </Navbar.Body> */}
-              {/* </Navbar.Offcanvas> */}
-            {/* </Navbar.Collapse> */}
-          {/* </Navbar.Toggle> */}
+          <Nav className="me-auto">
+            <NavDropdown className="mr-3" title="Product">
+              <NavDropdown.Item href="#">Overview</NavDropdown.Item>
+              <NavDropdown.Item href="#">Feautures</NavDropdown.Item>
+              <NavDropdown.Item href="#">Plans</NavDropdown.Item>
+            </NavDropdown>
+            <NavDropdown className="mr-3" title="Resources">
+              <NavDropdown.Item href="#">Solutions</NavDropdown.Item>
+              <NavDropdown.Item href="#">NewsRoom</NavDropdown.Item>
+            </NavDropdown>
+            <NavDropdown className="mr-3" title="Company">
+              <NavDropdown.Item href="#">About</NavDropdown.Item>
+              <NavDropdown.Item href="#">Team</NavDropdown.Item>
+              <NavDropdown.Item href="#">Careers</NavDropdown.Item>
+              <NavDropdown.Item href="#">Advertise</NavDropdown.Item>
+              <NavDropdown.Item href="#">Contact us</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+          <a href="#" className="hov">
+            Log in
+          </a>
+          <Button className="started">Get started</Button>
         </Container>
       </Navbar>
     </div>
